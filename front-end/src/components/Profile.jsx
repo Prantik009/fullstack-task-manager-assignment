26bb290a21412674507c271b9e6f1d613d5f44b0
Prantik Biswas <103238660+WebHault108@users.noreply.github.com>
2025-05-20 13:15:07 +0530

    Update Profile.jsx url update

--- front-end/src/components/Profile.jsx
+++ front-end/src/components/Profile.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const api_url = "http://localhost:4000";
+const api_url = "https://task-manager-backend-1ddi.onrender.com";
 
 const Profile = ({ setCurrentUser, onLogout }) => {
   const [profile, setProfile] = useState({ name: "", email: "" });
