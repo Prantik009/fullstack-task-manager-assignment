5f6601e6b74bfa87c6d414a35d6bb0f9866b6bfb
Prantik Biswas <103238660+Prantik009@users.noreply.github.com>
2025-05-25 09:38:33 +0530

    Update Profile.jsx

--- front-end/src/components/Profile.jsx
+++ front-end/src/components/Profile.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const api_url = "https://task-manager-backend-1ddi.onrender.com";
+const api_url = "https://fullstack-task-manager-assignment-backend.onrender.com";
 
 const Profile = ({ setCurrentUser, onLogout }) => {
   const [profile, setProfile] = useState({ name: "", email: "" });
